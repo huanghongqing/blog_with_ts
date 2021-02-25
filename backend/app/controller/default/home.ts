8d2eecadf1a57f19e511c36fd49852771265e0eb
huanghongqing <eclipseiscool@gmail.com>
2021-02-25 20:45:24 +0800

    front end --axios ajax get Index article list

--- backend/app/controller/default/home.ts
+++ backend/app/controller/default/home.ts
@@ -10,17 +10,17 @@ export default class HomeController extends Controller{
         
     }
     async getArticleList(){
-        let id= this.ctx.params.id | 1;
+        //let id= this.ctx.params.id;
         let sql= 'select article.id as id,'+
                   'article.title as title, ' +
                   'article.introduce as introduce, ' +
                   'article.article_content as content ,' +
-                  //"FROM_UNIXTIME(article.addTIme,'%Y-%m-%d %H:%i:%s') as addTime ," +
+                  "FROM_UNIXTIME(article.addTIme,'%Y-%m-%d %H:%i:%s') as addTime ," +
                   'article.view_count as view_count ,' +
                   'type.typeName as typeName ,'+
                   'type.id as typeId '+
-                  ' from article left join type on article.type_id=type.id '+
-                  ' where article.id='+id;
+                  ' from article left join type on article.type_id=type.id ';
+                  //' where article.id='+id;
         const result= await this.app.mysql.query(sql);
         this.ctx.body={data:result};
     }
