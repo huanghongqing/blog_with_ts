5ab6554d1a319db7cf8678bd8ee13b0e68b9feb0
huanghongqing <eclipseiscool@gmail.com>
2021-02-28 11:08:21 +0800

    admin:addArticle markdown support...

--- admin/src/Pages/AddArticle.tsx
+++ admin/src/Pages/AddArticle.tsx
@@ -10,7 +10,37 @@ const {Option} = Select //从select 接口里获取Option对象
 const {TextArea} =Input
 
 function AddArticle(){
-
+    const [articleId,setArticleId] = useState(0)  // 文章的ID，如果是0说明是新增加，如果不是0，说明是修改
+    const [articleTitle,setArticleTitle] = useState('')   //文章标题
+    const [articleContent , setArticleContent] = useState('')  //markdown的编辑内容
+    const [markdownContent, setMarkdownContent] = useState('预览内容') //html内容
+    const [introducemd,setIntroducemd] = useState()            //简介的markdown内容
+    const [introducehtml,setIntroducehtml] = useState('等待编辑') //简介的html内容
+    const [showDate,setShowDate] = useState()   //发布日期
+    const [updateDate,setUpdateDate] = useState() //修改日志的日期
+    const [typeInfo ,setTypeInfo] = useState([]) // 文章类别信息
+    const [selectedType,setSelectType] = useState(1) //选择的文章类别
+    const renderer=new marked.Renderer()
+    marked.setOptions({
+        renderer: renderer,
+        gfm: true,
+        pedantic: false,
+        sanitize: false,
+        breaks: false,
+        smartLists: true,
+        smartypants: false,
+      }); 
+      const changeContent = (e:any)=>{
+        setArticleContent(e.target.value)
+        let html=marked(e.target.value)
+        setMarkdownContent(html)
+      }
+ 
+      const changeIntroduce = (e:any)=>{
+         setIntroducemd(e.target.value)
+         let html=marked(e.target.value)
+         setIntroducehtml(html)
+      }
     return(
         <div>
             <Row gutter={5}>
@@ -34,12 +64,13 @@ function AddArticle(){
                     <br />
                     <Row gutter={10}>
                         <Col span={12}>
-                            <TextArea className="markdown-content" rows={23} placeholder="Blog content"> 
+                            <TextArea className="markdown-content" rows={23} placeholder="Blog content" 
+                                onChange={changeContent} 
+                            > 
                             </TextArea>
                         </Col>
                         <Col span={12}>
-                            <div className="show-html">
-                                html content.
+                            <div className="show-html" dangerouslySetInnerHTML = {{__html:markdownContent}}>
                             </div>
                         </Col>                        
                     </Row>
@@ -55,10 +86,10 @@ function AddArticle(){
                             <TextArea
                                 rows={9}
                                 placeholder="Blog introduce."
+                                onChange={changeIntroduce}
                             ></TextArea>
                             <br /><br />
-                            <div className="introduce-html">
-                                introduce html
+                            <div className="introduce-html" dangerouslySetInnerHTML = {{__html:'introduce:'+introducehtml}} >
                             </div>
                             <br /><br />
                         </Col>
