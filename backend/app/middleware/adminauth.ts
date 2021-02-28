6929031e8f46e48dada9d26cc74c435cffb76ced
huanghongqing <eclipseiscool@gmail.com>
2021-02-28 23:25:59 +0800

    backend: router guard init

---
+++ backend/app/middleware/adminauth.ts
@@ -0,0 +1,10 @@
+export default (options)=>{
+    return async function adminauth(ctx,next){ 
+        console.log(ctx.session.openId)
+        if(ctx.session.openId){ //通过openId是否存在判断是否登录
+            await next
+        }else{
+            ctx.body={data:'No Login.'}
+        }
+    }
+}
--- backend/app/router/admin.ts
+++ backend/app/router/admin.ts
@@ -1,6 +1,7 @@
 const router_admin = app=>{
     const { router,controller }=app;
-    router.get('/admin/index',controller.admin.main.index)
+    var adminauth= app.middleware.adminauth() //路由守卫
+    router.get('/admin/index',adminauth,controller.admin.main.index) //添加了路由守卫的路由，每次访问index都要验证是否登录
     router.post('/admin/checkLogin',controller.admin.main.checkLogin)
 }
 export default router_admin;
